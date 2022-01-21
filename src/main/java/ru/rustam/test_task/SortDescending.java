e1374ca0d87e3af8e393b2c99717aedbb6462c13
rustam26 <rustam.ru26@mail.ru>
2022-01-21 18:01:25 +0300

    Commit2

--- src/main/java/ru/rustam/test_task/SortDescending.java
+++ src/main/java/ru/rustam/test_task/SortDescending.java
@@ -1,9 +1,8 @@
 package ru.rustam.test_task;
 
-public class SortАscending extends Sort {
+public  class SortDescending extends Sort{
     @Override
-    protected void merge(String[] src1, int src1Start, String[] src2,
-                         int src2Start, String[] dest, int destStart, int size) {
+    protected void merge(String[] src1, int src1Start, String[] src2, int src2Start, String[] dest, int destStart, int size) {
 
         int index1 = src1Start;
         int index2 = src2Start;
@@ -15,7 +14,7 @@ public class SortАscending extends Sort {
         int iterationCount = src1End - src1Start + src2End - src2Start;
 
         for (int i = destStart; i < destStart + iterationCount; i++){
-            if (index1 < src1End && (index2 >= src2End || src1[index1].compareTo( src2[index2] ) < 0)){
+            if (index1 < src1End && (index2 >= src2End || src1[index1].compareTo(src2[index2]) > 0 )){
                 dest[i] = src1[index1];
                 index1++;
             }else {
@@ -23,13 +22,10 @@ public class SortАscending extends Sort {
                 index2++;
             }
         }
-
     }
 
     @Override
-    protected void merge(Integer[] src1, int src1Start, Integer[] src2,
-                         int src2Start, Integer[] dest, int destStart, int size) {
-
+    protected void merge(Integer[] src1, int src1Start, Integer[] src2, int src2Start, Integer[] dest, int destStart, int size) {
         int index1 = src1Start;
         int index2 = src2Start;
 
@@ -40,7 +36,7 @@ public class SortАscending extends Sort {
         int iterationCount = src1End - src1Start + src2End - src2Start;
 
         for (int i = destStart; i < destStart + iterationCount; i++){
-            if (index1 < src1End && (index2 >= src2End || src1[index1] < src2[index2] )){
+            if (index1 < src1End && (index2 >= src2End || src1[index1] > src2[index2] )){
                 dest[i] = src1[index1];
                 index1++;
             }else {
@@ -48,12 +44,11 @@ public class SortАscending extends Sort {
                 index2++;
             }
         }
-
     }
 
 
 //    @Override
-//    Integer[] merge(Integer[] a, Integer[] b) {
+//     Integer[] merge(Integer[] a, Integer[] b) {
 //
 //        Integer[] result = new Integer[a.length + b.length];
 //        int n = a.length;
@@ -61,7 +56,7 @@ public class SortАscending extends Sort {
 //
 //        int i = 0, j = 0, k = 0;
 //        while (i < n && j < m) {
-//            if (a[i] <= b[j]) {
+//            if (a[i] >= b[j]) {
 //                result[k] = a[i];
 //                i++;
 //            } else {
@@ -88,13 +83,14 @@ public class SortАscending extends Sort {
 //
 //    @Override
 //    String[] merge(String[] a, String[] b) {
+//
 //        String[] result = new String[a.length + b.length];
 //        int n = a.length;
 //        int m = b.length;
 //
 //        int i = 0, j = 0, k = 0;
 //        while (i < n && j < m) {
-//            if (a[i].compareTo(b[j]) <= 0) {
+//            if (a[i].compareTo(b[j]) >= 0) {
 //                result[k] = a[i];
 //                i++;
 //            } else {
@@ -118,6 +114,5 @@ public class SortАscending extends Sort {
 //
 //        return result;
 //    }
-
 
 }
